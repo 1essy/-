07575857a47932a9cda48d12a26e541c27194cbb
1essy <lestia495@yahoo.co.jp>
2022-06-15 11:54:07 +0000

    管理者側バリデーション変更

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,4 +1,5 @@
 class Admin::SearchesController < ApplicationController
+  before_action :authenticate_admin!
   def search
     @query = { address_or_category_or_customer_name_cont: params[:q], move_method_eq: params[:move_method_eq], smoking_area_eq: params[:smoking_area_eq], toilet_eq: params[:toilet_eq] }
     @search = Rest.ransack(@query)
