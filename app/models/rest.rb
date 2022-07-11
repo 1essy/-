05ad015ebcba45b5f805020d6ed074753311dad9
1essy <lestia495@yahoo.co.jp>
2022-07-11 09:39:22 +0000

    read.me編集

--- app/models/rest.rb
+++ app/models/rest.rb
@@ -54,7 +54,7 @@ class Rest < ApplicationRecord
       if notice.send_id == notice.receive_id
         notice.checked = true
       end
-      notice.save if notice.valid?
+      notice.save if notice.valid? 
     end
   end
 
@@ -65,7 +65,7 @@ class Rest < ApplicationRecord
     temp_ids.each do |temp_id|
       save_notice_rest_comment!(current_customer, rest_comment_id, temp_id['customer_id'])
     end
-    # まだ誰もコメントしていない場合は、投稿者に通知を送る 
+    # まだ誰もコメントしていない場合は、投稿者に通知を送る
     save_notice_rest_comment!(current_customer, rest_comment_id, customer_id) if temp_ids.blank?
   end
 
